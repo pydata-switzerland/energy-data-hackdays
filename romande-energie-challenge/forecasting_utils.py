a173bf008da05228184c4d0056562bc3a71fdffb
Gianr Lazz <gianroccolazzari@protonmail.com>
2026-05-06 16:12:25 +0200

    basic CV + grid search with `XGBRegressor`

---
+++ romande-energie-challenge/forecasting_utils.py
@@ -0,0 +1,162 @@
+"""basic module for forecasting basic functions"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import (
+    root_mean_squared_error,
+    mean_absolute_percentage_error,
+)
+import numpy as np
+
+
+def train_test_split(
+    features: pd.DataFrame,
+    targets: pd.DataFrame,
+    train_fraction: float = 0.7,
+    plot=False,
+):
+    """split the data into train, val and test sets,
+    according to the given fractions. optionally plot the split.
+
+    Args:
+        features (pd.DataFrame): feature data
+        targets (pd.DataFrame): target data
+        train_fraction (float, optional): fraction of data to use for training. Defaults to 0.7.
+        plot (bool, optional): whether to plot the split. Defaults to False.
+    Returns:
+        tuple: train, val and test sets for features and targets
+    """
+    X = features
+    y = targets
+    assert len(X) == len(y), (
+        "Features and targets must have the same number of samples"
+    )
+
+    train_size = int(train_fraction * len(X))
+
+    X_train = X.iloc[:train_size]
+    X_test = X.iloc[train_size:]
+
+    y_train = y.iloc[:train_size]
+    y_test = y.iloc[train_size:]
+
+    if plot:
+        # plot with different colors the targets
+        # in train, val and test sets to check the split
+
+        fig = plt.figure(figsize=(12, 6))
+        for i, target in enumerate(y_train.columns):
+            plt.plot(
+                X_train.index,
+                y_train[target],
+                label=f"Train - {target}",
+                alpha=0.5,
+            )
+            plt.plot(
+                X_test.index,
+                y_test[target],
+                label=f"Test - {target}",
+                alpha=0.5,
+            )
+            break  # only plot the first target for clarity
+        plt.xlabel("Time")
+        plt.ylabel("targets")
+        plt.title("Train, Validation, and Test Split")
+        plt.legend()
+        fig.show()
+
+    return X_train, y_train, X_test, y_test
+
+
+def run_CV(
+    model, X_train: pd.DataFrame, y_train: pd.DataFrame, tscv, verbose=True
+):
+    """run cross-validation
+
+    Args:
+        model: usual sklearn-like model with fit and predict methods
+        X_train (pd.DataFrame): training feature data
+        y_train (pd.DataFrame): training target data
+        tscv (generator): time series cross-validation splitter
+        verbose (bool, optional): whether to print details of the splits. Defaults to True.
+
+    Returns:
+        pd.DataFrame: DataFrame containing cross-validation metrics for each fold
+    """
+    assert hasattr(model, "fit"), "Model must have a fit method"
+    assert hasattr(model, "predict"), "Model must have a predict method"
+
+    cv_results = []
+
+    for fold, (train_idx, val_idx) in enumerate(tscv.split(X_train), start=1):
+        # show details of the splits
+        if verbose:
+            print(f"Fold {fold}:")
+            print(
+                f"  Train indices: {train_idx[0]} to {train_idx[-1]} (size: {len(train_idx)})"
+            )
+            print(
+                f"  Validation indices: {val_idx[0]} to {val_idx[-1]} (size: {len(val_idx)})"
+            )
+
+        X_tr, X_val = X_train.iloc[train_idx], X_train.iloc[val_idx]
+        y_tr, y_val = y_train.iloc[train_idx], y_train.iloc[val_idx]
+
+        model.fit(X_tr, y_tr)
+        y_val_pred = model.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_val_pred)
+        mape = mean_absolute_percentage_error(y_val, y_val_pred)
+        cv_results.append({"fold": fold, "rmse": rmse, "mape": mape})
+
+    cv_results_df = pd.DataFrame(cv_results)
+
+    print(
+        "CV mean metrics -> "
+        f"RMSE: {cv_results_df['rmse'].mean():.4f}, "
+        f"MAPE: {cv_results_df['mape'].mean():.4f}"
+    )
+
+    return cv_results_df
+
+
+def plot_test_pred(y_test: pd.DataFrame, y_pred_test: np.ndarray):
+
+    for i, target in enumerate(y_test.columns):
+        plt.figure(figsize=(10, 5))
+        plt.plot(y_test.index, y_test[target], label="True")
+        plt.plot(y_test.index, y_pred_test[:, i], label="Predicted")
+        plt.xlabel("Time")
+        plt.ylabel(target)
+        plt.title(f"{target} -- Test Set")
+        plt.legend()
+        plt.show()
+
+
+def test_model(
+    model, X_test: pd.DataFrame, y_test: pd.DataFrame, plot: bool = False
+):
+    """Test a model on the given test set and optionally plot the predictions.
+
+    Args:
+        model: the model to test, must have a predict method
+        X_test (pd.DataFrame): feature data for testing
+        y_test (pd.DataFrame): target data for testing
+        plot (bool, optional): whether to plot the predictions. Defaults to False.
+
+    """
+
+    assert hasattr(model, "predict"), "Model must have a predict method"
+    y_pred_test = model.predict(X_test)
+
+    # show metrics per target
+    for i, target in enumerate(y_test.columns):
+        target_rmse = root_mean_squared_error(y_test[target], y_pred_test[:, i])
+        target_mape = mean_absolute_percentage_error(
+            y_test[target], y_pred_test[:, i]
+        )
+        print(f"{target} -- Test RMSE: {target_rmse:.3f}")
+        print(f"{target} -- Test MAPE: {target_mape:.3f}")
+
+    if plot:
+        plot_test_pred(y_test, y_pred_test)
--- romande-energie-challenge/tetouan_city_power.py
+++ romande-energie-challenge/tetouan_city_power.py
@@ -3,27 +3,196 @@
 
 # %% [markdown]
 ## imports
+from pathlib import Path
+import sys
+
+sys.path.append(str((Path.cwd() / "romande-energie-challenge").resolve()))
+
+
 from ucimlrepo import fetch_ucirepo
+import pandas as pd
+import plotly.io as pio
+from xgboost import XGBRegressor
 
-# %% [markdown]
-# fetch and prepare dataset
-tetouan_power = fetch_ucirepo(id=849)
-
-# data (as pandas dataframes)
-X = tetouan_power.data.features
-y = tetouan_power.data.targets
-
-# metadata
-print(tetouan_power.metadata)
-
-# variable information
-print(tetouan_power.variables)
-
-
-# %% [markdown]
-# ## quick  data exploration
-
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 
 # %%
 
+from forecasting_utils import train_test_split, test_model, run_CV
+
+
+# %load_ext autoreload
+# %autoreload 1
+# %aimport forecasting_utils
+
+# %% [markdown]
+# ## fetch and explore dataset
+tetouan_power = fetch_ucirepo(id=849)
+
+
+# overview of the raw dataset
+print(type(tetouan_power))
+for k in tetouan_power.keys():
+    print(f"{k}: {type(tetouan_power[k])}")
+    print(f"{k}: {tetouan_power[k].keys()}")
+
+# %% [markdown]
+# ### data features exploration
+
+print(type(tetouan_power.data.features))
+print(tetouan_power.data.features.info())
+display(tetouan_power.data.features.describe())
+display(tetouan_power.data.features.head())
+
+# %% [markdown]
+#  ### data targets exploration
+print(type(tetouan_power.data.targets))
+print(tetouan_power.data.targets.info())
+display(tetouan_power.data.targets.describe())
+display(tetouan_power.data.targets.head())
+
+
+# %%
+# set datetime index
+
+
+tetouan_power.data.features.index = pd.DatetimeIndex(
+    tetouan_power.data.features["DateTime"]
+)
+# we can drop the DateTime column as it is now the index
+tetouan_power.data.features.drop(columns=["DateTime"], inplace=True)
+
+tetouan_power.data.targets.set_index(
+    tetouan_power.data.features.index, inplace=True
+)
+# %% [markdown]
+# ## quick  data exploration
+
+# %% [markdown]
+# ### features scatter_matrix
+
+pd.options.plotting.backend = "matplotlib"
+
+pd.plotting.scatter_matrix(tetouan_power.data.features, figsize=(10, 10), s=3)
+
+# %% [markdown]
+# #### notes
+
+# is there some issues with wind speed ? the distribution is  basically bimodal
+
+# %% [markdown]
+# ### line plots
+
+INTERACTIVE = False
+if INTERACTIVE:
+    pd.options.plotting.backend = "plotly"
+
+    fig = tetouan_power.data.features.plot.line(title="features line plot")
+    pio.show(fig)
+
+    fig = tetouan_power.data.targets.plot.line(title="targets line plot")
+    pio.show(fig)
+else:
+    pd.options.plotting.backend = "matplotlib"
+    tetouan_power.data.features.plot.line(
+        figsize=(10, 5), alpha=0.3, title="features line plot"
+    )
+    tetouan_power.data.targets.plot.line(
+        figsize=(10, 5), alpha=0.3, title="targets line plot"
+    )
+
+# %% [markdown]
+# ## Forecasting task
+
+# %% [markdown]
+# ### feature preparation
+
+# %%
+# add temporal features: hour of day, day of week, month
+tetouan_power.data.features["hour"] = tetouan_power.data.features.index.hour
+tetouan_power.data.features["dayofweek"] = (
+    tetouan_power.data.features.index.dayofweek
+)
+tetouan_power.data.features["month"] = tetouan_power.data.features.index.month
+
+# %% [markdown]
+# ### Split the data
+
+# we try keeping only the most important features, but it makes the performance worse,
+# so we keep all features for now
+
+X_train, y_train, X_test, y_test = train_test_split(
+    tetouan_power.data.features, tetouan_power.data.targets, plot=True
+)
+
+
+# %% [markdown]
+# ### Assess a XGBoost regressor (multioutput)
+# relevants docs:
+# https://xgboost.readthedocs.io/en/stable/treemethod.html
+# https://xgboost.readthedocs.io/en/stable/tutorials/multioutput.html
+# https://xgboost.readthedocs.io/en/latest/python/python_api.html#module-xgboost.sklearn
+model = XGBRegressor(
+    tree_method="hist",
+    random_state=42,
+    multi_strategy="multi_output_tree",
+    n_estimators=100,
+    max_depth=6,
+    learning_rate=0.1,
+)
+model.fit(X_train, y_train)
+# %% [markdown]
+# ### feature importance
+
+feature_importances = model.feature_importances_
+feature_names = X_train.columns
+feature_importance_df = pd.DataFrame({
+    "feature": feature_names,
+    "importance": feature_importances,
+}).sort_values(by="importance", ascending=False)
+print(feature_importance_df)
+
+# %% [markdown]
+# ### Cross validation evaluation
+
+tscv = TimeSeriesSplit(n_splits=5, test_size=int(len(X_train) * 0.2), gap=0)
+
+cv_results = run_CV(model, X_train, y_train, tscv=tscv)
+display(cv_results)
+
+# %% [markdown]
+# ### Hyperparameter tuning with CV
+
+param_grid = {
+    "n_estimators": [50, 70, 100],
+    "max_depth": [6, 8],
+    "learning_rate": [0.1],  # from prev. exp.
+    # "learning_rate": [0.01, 0.1, 0.2]
+}
+grid_search = GridSearchCV(
+    estimator=XGBRegressor(
+        tree_method="hist", random_state=42, multi_strategy="multi_output_tree"
+    ),
+    param_grid=param_grid,
+    cv=tscv,
+    scoring="neg_mean_squared_error",
+    n_jobs=-1,
+    verbose=1,
+)
+grid_search.fit(X_train, y_train)
+
+# best hyperparameters
+print("Best Hyperparameters:", grid_search.best_params_)
+
+# %% [markdown]
+# ### retrain and test best model
+
+best_model = grid_search.best_estimator_
+best_model.fit(X_train, y_train)
+
+# test the best model on the test set
+test_model(best_model, X_test, y_test, plot=True)
+
+# %%
+# performance is still quite bad...room for improvement!
 # %%
